653bc28e774d1d70edaed8478df182f0b428ef9c
nktoan1905 <nguyenkhanhtoan1905>
2021-03-09 18:35:21 +0700

    update

--- java_oop_lap/src/lab02/ArrayCalculate.java
+++ java_oop_lap/src/lab02/ArrayCalculate.java
@@ -2,27 +2,38 @@ package lab02;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayCalculate {
-    public static void main(String[] args) {
-        int []array = {1789,2035,1899,1456,2013};
-        int s = 0, avg = 0;
 
-        System.out.println("Array elements: ");
-        for (int a: array){
-            System.out.println(a);
+    public static void main(String[] args) {
+        int[] array = new int[1000];
+        int i = 0,sum=  0;
+        double avg = 0;
+        String a;
+        String regex = "\\d+";
+        do {
+            a = new Scanner(System.in).nextLine();
+            if(a.matches("\\D++")){
+                break;
+            }
+            array[i++] = Integer.parseInt(a);
+        } while (a.matches(regex));
+        for (int j = i; j < 1000; j++) {
+            array[j] = (int) (Math.random() * 1000) + 0;
         }
-        for (int i = 0; i < array.length; i++) {
-            s += array[i];
+        for (int j = 0; j < 1000; j++) {
+            sum+=array[j];
         }
-        System.out.println("Sum: "+ s);
-        avg = s/array.length;
-        System.out.println("Avg: "+avg);
-        System.out.println("Sort: ");
+        avg = sum/1000;
         Arrays.sort(array);
-        for (int a: array){
-            System.out.println(a);
+
+        for (int b: array
+             ) {
+            System.out.println(b);
         }
+        System.out.println("Sum = "+sum);
+        System.out.println("Avg = "+avg);
 
 
 
