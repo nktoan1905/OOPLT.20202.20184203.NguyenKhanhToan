f5847c2de234511765abe73f560c9200e2f7eb4a
nktoan1905 <nguyenkhanhtoan1905>
2021-03-09 18:47:44 +0700

    update

--- java_oop_lap/src/lab02/ArrayCalculate.java
+++ java_oop_lap/src/lab02/ArrayCalculate.java
@@ -7,34 +7,47 @@ import java.util.Scanner;
 public class ArrayCalculate {
 
     public static void main(String[] args) {
-        int[] array = new int[1000];
-        int i = 0,sum=  0;
+
+        int i = 0, sum = 0, size;
         double avg = 0;
         String a;
         String regex = "\\d+";
+        System.out.println("Nhập vào số phần tử của mảng");
+        size = new Scanner(System.in).nextInt();
+        int[] array = new int[size];
         do {
             a = new Scanner(System.in).nextLine();
-            if(a.matches("\\D++")){
+            if(i>size){
+                break;
+            }
+            if (a.matches("\\D++")) {
                 break;
             }
             array[i++] = Integer.parseInt(a);
         } while (a.matches(regex));
-        for (int j = i; j < 1000; j++) {
+
+        System.out.println("các phần tử nhập vào");
+        for (int j = 0; j < i; j++) {
+            System.out.println(array[j]);
+        }
+
+        for (int j = i; j < size; j++) {
             array[j] = (int) (Math.random() * 1000) + 0;
         }
-        for (int j = 0; j < 1000; j++) {
-            sum+=array[j];
+
+        for (int j = 0; j < size; j++) {
+            sum += array[j];
         }
-        avg = sum/1000;
+
+        avg = sum / size;
         Arrays.sort(array);
 
-        for (int b: array
-             ) {
-            System.out.println(b);
+        System.out.println("sau khi sắp xếp:");
+        for (int j = 0; j < size; j++) {
+            System.out.println(array[j]);
         }
-        System.out.println("Sum = "+sum);
-        System.out.println("Avg = "+avg);
-
+        System.out.println("Sum = " + sum);
+        System.out.println("Avg = " + avg);
 
 
     }
