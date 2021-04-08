25494fe12e7dd99f11a7b4fb1f0dc1f459e155bf
nktoan1905 <nguyenkhanhtoan1905>
2021-04-08 12:49:05 +0700

    update

---
+++ AimsProjectVerlap07/src/hust/soict/hedspi/aims/DiskTest.java
@@ -0,0 +1,7 @@
+package hust.soict.hedspi.aims;
+
+
+
+public class DiskTest {
+
+}
---
+++ AimsProjectVerlap07/src/hust/soict/hedspi/aims/disc/DiscSwap.java
@@ -0,0 +1,6 @@
+package hust.soict.hedspi.aims.disc;
+
+public class DiscSwap {
+    public DigitalVideoDisc c;
+    public DiscSwap(DigitalVideoDisc c)   {this.c = c;}
+}
